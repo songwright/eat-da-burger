d64215b469c3b11ca27e90945ed73c90a568e86b
Ramon Sanchez <songwright66@gmail.com>
2019-10-28 10:28:59 -0700

    Updated model file burger.js with orm properties

--- models/burger.js
+++ models/burger.js
@@ -5,16 +5,25 @@ var burger = {
     //
     // return all burgers
     //
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
   },
   create: function(name, cb) {
     //
     // create burger
     //
+    orm.create("burgers", name, cb, function(res) {
+      cb(res);
+    });
   },
   update: function(id, cb) {
     //
     // modify burger
     //
+    orm.update("burgers", id, cb, function(res) {
+      cb(res);
+    })
   }
 };
 
