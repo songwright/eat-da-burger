89b62405101dffa4cc85ddac7d1dcf8c70fa71af
Ramon Sanchez <songwright66@gmail.com>
2019-10-28 20:05:49 -0700

    Fixed error in burgerController.js

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -42,7 +42,7 @@ router.put("/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.devoured
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
